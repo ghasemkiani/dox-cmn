45965ad42502b36524c0ec0989600d5a11e98099
Ghasem Kiani <ghasemkiani@gmail.com>
2024-02-14 11:56:41 +0330

    update

--- atdir.js
+++ atdir.js
@@ -6,6 +6,7 @@ import {Component} from "@ghasemkiani/dox";
 class AtDir extends Component {
 	async toRender(node) {
 		let component = this;
+		let {x} = component;
 		let {context} = component;
 		let uri = x.attr(component.node, "uri");
 		let dirname = path.join(context.dirname, uri);
